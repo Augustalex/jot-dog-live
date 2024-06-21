492e385c1238e4f16f30c0ee30ce58bd2e9c11d1
August Alexandersson <augustalexandersson@gmail.com>
2024-06-22 00:32:53 +0200

    Use ssl for server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ import {Server} from '@hocuspocus/server'
 import {SQLite} from '@hocuspocus/extension-sqlite'
 
 const server = Server.configure({
-    port: 80,
+    port: 443,
+    ssl: {
+        key: '/etc/letsencrypt/live/live.jot.dog/privkey.pem',
+        cert: '/etc/letsencrypt/live/live.jot.dog/fullchain.pem'
+    },
 
     async onConnect() {
         console.log('Running server.')
